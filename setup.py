304c3c8cfd617ba9fac0127d2f5361b62c3e25db
Robby Dermody <robbyd@paramise.com>
2012-01-12 12:26:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     package_data={
         "minidetector": ["search_strings.txt"]
     },
+    include_package_data=True,
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
